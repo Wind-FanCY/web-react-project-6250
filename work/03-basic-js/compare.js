9b19e428c84a33771f9761eecc71c0a4f01d2f97
wind-fcy <wind_fcy@outlook.com>
2025-01-28 01:15:09 -0800

    these changes compare.js

--- work/03-basic-js/compare.js
+++ work/03-basic-js/compare.js
@@ -1,10 +1,30 @@
 "use strict";
-/* DO NOT MODIFY EXCEPT WHERE ALLOWED */
-module.exports = compare; // DO NOT MODIFY - USED FOR TESTING
 
-function compare( word, guess ) {  // DO NOT MODIFY THIS LINE
+module.exports = compare; 
 
-/* YOU MAY MODIFY THE LINES BELOW */
+function compare(word, guess) {
 
-  return 0; // this line is wrong
+  let sameLetter = 0;
+  const wordNum = {};
+
+  for (let w of word) {
+    const wUpper = w.toUpperCase();
+    
+    if (!(wUpper in wordNum)) {
+      wordNum[wUpper] = 0;
+    }
+
+    wordNum[wUpper] += 1;
+  }
+
+  for (let g of guess) {
+    const gUpper = g.toUpperCase();
+
+    if (gUpper in wordNum && wordNum[gUpper] > 0) {
+      sameLetter += 1;
+      wordNum[gUpper] -= 1;
+    }
+  }
+  
+  return sameLetter; 
 }
